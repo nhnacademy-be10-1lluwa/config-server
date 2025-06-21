ee67b4491ac967fe0369e83d804e719746f90cfc
pear-c <100738513+pear-c@users.noreply.github.com>
2025-06-21 15:10:22 +0900

    infra: Config Server 설정 (#3) (#4)

--- src/test/java/com/nhnacademy/illuwa/ConfigServerApplicationTests.java
+++ src/test/java/com/nhnacademy/illuwa/ConfigServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IlluwaApplicationTests {
+class ConfigServerApplicationTests {
 
     @Test
     void contextLoads() {
